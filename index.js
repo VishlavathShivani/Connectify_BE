a3c03d3812f63ff0bc4c58b22b0c2b9c11341e22
VishlavathShivani <107530417+VishlavathShivani@users.noreply.github.com>
2023-10-19 11:52:23 +0530

    changes

--- index.js
+++ index.js
@@ -51,6 +51,11 @@ app.use("/posts",postRoutes);
 
 
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
 
 
 const PORT=process.env.PORT || 4000;
